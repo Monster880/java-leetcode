6cdadaa8c4b62b72da6d09c5571bc43f6382b6ea
liding <539561425@qq.com>
2021-05-01 13:22:18 +0800

    feature: 1524题用前缀和看奇偶

---
+++ leetcode/1524.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        final int MODULO = 1000000007;
+        int odd = 0, even = 1;
+        int subarrays = 0;
+        int sum = 0;
+        int length = arr.length;
+        for(int i=0;i<length;i++){
+            sum += arr[i];
+            subarrays = (subarrays + (sum % 2 == 0 ? odd : even)) % MODULO;
+            if(sum % 2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        return subarrays;
+    }
+}
