2de7bb61bfe1ad40fed1aaeedb891401b2754869
liding <539561425@qq.com>
2021-05-06 12:16:48 +0800

    feature: 1372题使用递归

---
+++ leetcode/1372.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int[] dfs(TreeNode root){
+        if(root == null){
+            return new int[]{-1, -1, -1};
+        }
+        int[] left = dfs(root.left), right = dfs(root.right);
+        return new int[]{left[1]+1, right[0]+1, 
+                    Math.max(Math.max(left[1]+1, right[0]+1),
+                    Math.max(left[2],right[2]))
+        };
+    }
+    public int longestZigZag(TreeNode root) {
+        return dfs(root)[2];
+    }
+}
