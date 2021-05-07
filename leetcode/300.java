ca15f292c473078922c2179ebf220e250d02b73f
liding <539561425@qq.com>
2021-05-07 22:49:03 +0800

    feature: 动态规划

---
+++ leetcode/300.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxn = 1;
+        for(int i=1;i<nums.length;i++){
+            dp[i] =1;
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            maxn = Math.max(maxn, dp[i]);
+        }
+        return maxn;
+    }
+}
