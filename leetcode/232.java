b4140c4cd08a3ab8e6c21003af807cd908a38864
liding <539561425@qq.com>
2021-05-31 23:30:50 +0800

    feature: 双栈模拟队列

---
+++ leetcode/232.java
@@ -0,0 +1,41 @@
+class MyQueue {
+
+    private Stack<Integer> s;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        Stack<Integer> temp = new Stack();
+        for(; !s.isEmpty(); temp.push(s.pop()));
+        s.push(x);
+        for(; !temp.isEmpty(); s.push(temp.pop()));
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return s.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return s.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
