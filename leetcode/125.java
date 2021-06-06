3bf50f93f8880fe02d276e19d8e8fd32065fb22f
liding <539561425@qq.com>
2021-06-06 23:13:09 +0800

    feature: 双指针

---
+++ leetcode/125.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int left = 0, right = n -1;
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                ++left;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                --right;
+            }
+            if(left < right){
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
