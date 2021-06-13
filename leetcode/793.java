e79993fe2d51357c4b26f72af9a4813ba2c0e3e2
liding <539561425@qq.com>
2021-06-13 19:26:42 +0800

    fix: 修改命名

--- leetcode/793.java
+++ leetcode/793.java
@@ -1,18 +1,18 @@
 class Solution {
     public int preimageSizeFZF(long k) {
-        long lo = k, hi = 10*k+1;
-        while(lo < hi){
-            long mi = lo + (hi-lo)/2;
-            long zmi = zeta(mi);
-            if(zmi == k) return 5;
-            else if(zmi < k) lo = mi + 1;
-            else hi = mi;
+        long left = k, right = 10*k+1;
+        while(left < right){
+            long mid = (left + right) >> 1;
+            long sum = count(mid);
+            if(sum == k) return 5;
+            else if(sum < k) left = mid + 1;
+            else right = mid;
         }
         return 0;
     }
 
-    public long zeta(long x){
+    public long count(long x){
         if(x == 0) return 0;
-        return x/5 + zeta(x/5);
+        return x/5 + count(x/5);
     }
 }
