d27b37715210b6b8231792d66d651a1a8d457654
liding <539561425@qq.com>
2021-06-19 11:52:03 +0800

    fix: 修改代码

--- leetcode/707.java
+++ leetcode/707.java
@@ -1,7 +1,7 @@
-public class ListNode {
+public class ListNode{
     int val;
     ListNode next;
-    ListNode(int x){ val = x; }
+    ListNode(int x){val = x;}
 }
 
 class MyLinkedList {
@@ -16,9 +16,9 @@ class MyLinkedList {
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
         if(index < 0 || index >= size) return -1;
-        ListNode curr = head;
-        for(int i=0; i<index + 1; i++) curr = curr.next;
-        return curr.val;
+        ListNode cur = head;
+        for(int i=0; i<index+1; i++) cur = cur.next;
+        return cur.val;
     }
     
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
@@ -28,7 +28,7 @@ class MyLinkedList {
     
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
-        addAtIndex(size, val);
+        addAtIndex(size,val);
     }
     
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
@@ -36,20 +36,20 @@ class MyLinkedList {
         if(index > size) return;
         if(index < 0) index = 0;
         ++size;
-        ListNode pred = head;
-        for(int i=0; i < index; i++) pred = pred.next;
-        ListNode toAdd = new ListNode(val);
-        toAdd.next = pred.next;
-        pred.next = toAdd;
+        ListNode pre = head;
+        for(int i=0; i<index; i++) pre = pre.next;
+        ListNode temp = new ListNode(val);
+        temp.next = pre.next;
+        pre.next = temp;
     }
     
     /** Delete the index-th node in the linked list, if the index is valid. */
     public void deleteAtIndex(int index) {
         if(index < 0 || index >= size) return;
         size--;
-        ListNode pred = head;
-        for(int i=0; i<index; i++) pred = pred.next;
-        pred.next = pred.next.next;
+        ListNode pre = head;
+        for(int i=0; i< index; i++) pre = pre.next;
+        pre.next = pre.next.next;
     }
 }
 
