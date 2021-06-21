887cafb1e4721008a44195c76481834d6b12157b
liding <539561425@qq.com>
2021-06-21 08:01:00 +0800

    feature: 前序遍历

---
+++ leetcode/144.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void dfs(TreeNode root, List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        ans.add(root.val);
+        dfs(root.left, ans);
+        dfs(root.right, ans);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans);
+        return ans;
+    }
+}
