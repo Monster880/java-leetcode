921d7cee8c90202c450619834272b1a455e376b3
liding <539561425@qq.com>
2021-07-04 17:06:35 +0800

    feature: pdd4

--- leetcode/pdd4.java
+++ leetcode/pdd4.java
@@ -15,41 +15,42 @@ public class Main {
 		}
 	}
 	public static void main(String[] args){
-		Scanner in=new Scanner(System.in);
-		int N=in.nextInt();
-		int M=in.nextInt();
-		in.nextLine();
-		char[][] G=new char[N][M];
-		for(int i=0;i<N;i++){
-			G[i]=in.nextLine().toCharArray();
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		int m=scanner.nextInt();
+		scanner.nextLine();
+		char[][] Graph=new char[n][m];
+		for(int i=0;i<n;i++){
+			Graph[i]=scanner.nextLine().toCharArray();
 		}
-		for(int i=0;i<N;i++){
-			for(int j=0;j<M;j++){
-				if(G[i][j]=='2'){
-					System.out.println(bfs(i,j,N,M,G));
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(Graph[i][j]=='2'){
+					System.out.println(bfs(i,j,n,m,Graph));
 					return;
 				}
 			}
 		}
 	}
-	private static int bfs(int si, int sj,int N,int M,char[][] G) {
+	private static int bfs(int xi, int yj,int n,int m,char[][] Graph) {
 		Queue<Node> queue=new LinkedList<>();
-		int[][][] mp=new int[101][101][1025];
+		int[][][] temp=new int[101][101][1025];
 		int[][] next={{-1,0},{0,-1},{1,0},{0,1}};
 		
-		queue.offer(new Node(si,sj,0,0));
+		queue.offer(new Node(xi,yj,0,0));
 		while(!queue.isEmpty()){
 			Node node=queue.poll();
 			for(int i=0;i<4;i++){
 				int x=node.x+next[i][0];
 				int y=node.y+next[i][1];
 				int key=node.key;
-				if(x<0||x>=N||y<0||y>=M||G[x][y]=='0') continue;
-				else if(G[x][y]=='3') return node.step+1;
-				else if(G[x][y]<='z'&&G[x][y]>='a') key=key|(1<<(G[x][y]-'a'));
-				else if(G[x][y]<='Z'&&G[x][y]>='A'&&(key&(1<<(G[x][y]-'A')))==0) continue;
-				if(mp[x][y][key]==0){
-					mp[x][y][key]=1;
+                
+				if(x<0||x>=n||y<0||y>=m||Graph[x][y]=='0') continue;
+				else if(Graph[x][y]=='3') return node.step+1;
+				else if(Graph[x][y]<='z'&&Graph[x][y]>='a') key=key|((1<<Graph[x][y]-'a'));
+				else if(Graph[x][y]<='Z'&&Graph[x][y]>='A'&&(key&(1<<(Graph[x][y]-'A')))==0) continue;
+				if(temp[x][y][key]==0){
+					temp[x][y][key]=1;
 					queue.offer(new Node(x,y,key,node.step+1));
 				}
 				
