172bccca85237581c3a3d41ce867efe27014d23b
liding <539561425@qq.com>
2021-07-12 23:39:13 +0800

    feature: 修改代码

--- leetcode/209.java
+++ leetcode/209.java
@@ -4,18 +4,19 @@ class Solution {
         if(n == 0){
             return 0;
         }
-        int ans = Integer.MAX_VALUE;
-        int start = 0, end = 0;
+        int start = 0;
+        int end = 0;
         int sum = 0;
+        int ans = Integer.MAX_VALUE;
         while(end < n){
             sum += nums[end];
             while(sum >= target){
-                ans = Math.min(ans, end - start + 1);
                 sum -= nums[start];
+                ans = Math.min(ans, end - start+ 1);
                 start++;
             }
             end++;
         }
-        return ans == Integer.MAX_VALUE ? 0 : ans; 
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
