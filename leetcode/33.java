38333d238eeed9565bef0c08a6108982d80ee2c0
liding <539561425@qq.com>
2021-07-18 17:47:37 +0800

    feature: 遍历

---
+++ leetcode/33.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int result=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                result=i;
+                break;
+            }else{
+                 result=-1;
+            }
+
+        }
+        return result;
+    }
+}
