ebca3bb85253fbf2b10f2256a151324e699f898e
liding <539561425@qq.com>
2021-07-18 18:19:15 +0800

    fix: 修改代码

--- leetcode/34.java
+++ leetcode/34.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int leftIndex = binarySearch(nums, target, true);
-        int rightIndex = binarySearch(nums, target, false) -1;
+        int rightIndex = binarySearch(nums, target, false) - 1;
         if(leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] == target && nums[rightIndex] == target){
             return new int[]{leftIndex, rightIndex};
         }
-        return new int[]{-1, -1};
+        return new int[]{-1,-1};
     }
-
+    
     public int binarySearch(int[] nums, int target, boolean lower){
         int left = 0, right = nums.length - 1, ans = nums.length;
         while(left <= right){
-            int mid = (left + right) / 2;
+            int mid = (left + right) >> 1;
             if(nums[mid] > target || (lower && nums[mid] >= target)){
-                right = mid - 1;
+                right = mid -1;
                 ans = mid;
             }else{
                 left = mid + 1;
