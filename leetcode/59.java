46140073b4df8d3486082db5214ec0b8c9a45436
liding <539561425@qq.com>
2021-07-20 23:07:48 +0800

    fix: 修改代码

--- leetcode/59.java
+++ leetcode/59.java
@@ -4,11 +4,10 @@ class Solution {
         int curNum = 1;
         int[][] matrix = new int[n][n];
         int row = 0, column = 0;
-        int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; //右下左上
+        int[][] direction = {{0,1}, {1,0}, {0,-1}, {-1,0}};
         int directionIndex = 0;
         while(curNum <= maxNum){
-            matrix[row][column] = curNum;
-            curNum++;
+            matrix[row][column] = curNum++;
             int nextRow = row + direction[directionIndex][0], nextColumn = column + direction[directionIndex][1];
             if(nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0){
                 directionIndex = (directionIndex + 1) % 4;
