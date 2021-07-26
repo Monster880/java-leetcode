19bf9ec723d5572e84c0564156af33c8eeeed36e
liding <539561425@qq.com>
2021-07-26 23:40:50 +0800

    fix: 修改代码

--- leetcode/131.java
+++ leetcode/131.java
@@ -1,4 +1,5 @@
 class Solution {
+
     boolean[][] f;
     List<List<String>> res = new ArrayList<List<String>>();
     List<String> ans = new ArrayList<String>();
@@ -7,30 +8,28 @@ class Solution {
     public List<List<String>> partition(String s) {
         n = s.length();
         f = new boolean[n][n];
-        for (int i = 0; i < n; ++i) {
+        for(int i=0; i<n; i++){
             Arrays.fill(f[i], true);
         }
-
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1; j<n; j++){
+                f[i][j] = s.charAt(i) == s.charAt(j) && f[i+1][j-1];
             }
         }
-
         dfs(s, 0);
         return res;
     }
 
-    public void dfs(String s, int i) {
-        if (i == n) {
+    private void dfs(String s, int i){
+        if(i == n){
             res.add(new ArrayList<String>(ans));
             return;
         }
-        for (int j = i; j < n; ++j) {
-            if (f[i][j]) {
-                ans.add(s.substring(i, j + 1));
-                dfs(s, j + 1);
-                ans.remove(ans.size() - 1);
+        for(int j=i;j<n;j++){
+            if(f[i][j]){
+                ans.add(s.substring(i, j+1));
+                dfs(s,j+1);
+                ans.remove(ans.size()-1);
             }
         }
     }
