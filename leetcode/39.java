1a2d244f0b0c78cdaa47ac2796afd92836747af0
liding <539561425@qq.com>
2021-07-27 12:39:54 +0800

    feature: dfs

---
+++ leetcode/39.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, candidates, target, new ArrayList<Integer>(), 0);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, int[] candidates, int target, ArrayList<Integer> temp, int index){
+        if(index == candidates.length){
+            return;
+        }
+        if(target == 0){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if(target >= candidates[index]){
+            temp.add(candidates[index]);
+            backtrack(res, candidates, target-candidates[index], temp, index);
+            temp.remove(temp.size()-1);
+        }
+        backtrack(res, candidates, target, temp, index+1);
+    }
+}
