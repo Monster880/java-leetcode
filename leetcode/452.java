3d943dc9e448a282e17297cd8b5c5486407db0f5
liding <539561425@qq.com>
2021-07-30 12:13:28 +0800

    fix: 修改代码

--- leetcode/452.java
+++ leetcode/452.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) {
+        if(points.length == 0){
             return 0;
         }
-        Arrays.sort(points, new Comparator<int[]>() {
-            public int compare(int[] point1, int[] point2) {
-                if (point1[1] > point2[1]) {
+        Arrays.sort(points, new Comparator<int[]>(){
+            public int compare(int[] point1, int[] point2){
+                if(point1[1] > point2[1]){
                     return 1;
-                } else if (point1[1] < point2[1]) {
+                }else if(point1[1] < point2[1]){
                     return -1;
-                } else {
+                }else {
                     return 0;
                 }
             }
         });
         int pos = points[0][1];
         int ans = 1;
-        for (int[] balloon: points) {
-            if (balloon[0] > pos) {
+        for(int[] balloon : points){
+            if(balloon[0] > pos){
+                ans++;
                 pos = balloon[1];
-                ++ans;
             }
         }
         return ans;
