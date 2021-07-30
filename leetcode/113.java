620f6f8615f545ad1a3a49a9fb5547d8b1a230ac
liding <539561425@qq.com>
2021-07-30 23:53:14 +0800

    fix: 修改代码

--- leetcode/113.java
+++ leetcode/113.java
@@ -14,12 +14,12 @@
  * }
  */
 class Solution {
-    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    List<List<Integer>> res = new LinkedList<List<Integer>>();
     Deque<Integer> path = new LinkedList<Integer>();
 
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         dfs(root, targetSum);
-        return ret;
+        return res;
     }
 
     public void dfs(TreeNode root, int targetSum) {
@@ -29,7 +29,7 @@ class Solution {
         path.offerLast(root.val);
         targetSum -= root.val;
         if (root.left == null && root.right == null && targetSum == 0) {
-            ret.add(new LinkedList<Integer>(path));
+            res.add(new LinkedList<Integer>(path));
         }
         dfs(root.left, targetSum);
         dfs(root.right, targetSum);
