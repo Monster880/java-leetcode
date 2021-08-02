a265ad8c702f9f3f2ed3e9b3a76fd159629e7ed7
liding <539561425@qq.com>
2021-08-02 23:53:04 +0800

    fix: 修改代码

--- leetcode/416.java
+++ leetcode/416.java
@@ -4,18 +4,14 @@ class Solution {
         if (n < 2) {
             return false;
         }
-        int sum = 0, maxNum = 0;
+        int sum = 0;
         for (int num : nums) {
             sum += num;
-            maxNum = Math.max(maxNum, num);
         }
         if (sum % 2 != 0) {
             return false;
         }
         int target = sum / 2;
-        if (maxNum > target) {
-            return false;
-        }
         boolean[] dp = new boolean[target + 1];
         dp[0] = true;
         for (int i = 0; i < n; i++) {
