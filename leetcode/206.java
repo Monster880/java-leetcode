86eb5f41065a674f00e2e89754b765083c00c902
liding <539561425@qq.com>
2021-08-05 22:39:16 +0800

    feature: 反转链表

--- leetcode/206.java
+++ leetcode/206.java
@@ -1,25 +1,11 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    private Pair<ListNode, ListNode> reverse(ListNode head){
-        if(head == null || head.next == null){
-            return new Pair<>(head, head);
-        }
-        Pair<ListNode, ListNode> temp = reverse(head.next);
-        temp.getValue().next = head;
-        head.next = null;
-        return new Pair<>(temp.getKey(), head);
-    }
-    
     public ListNode reverseList(ListNode head) {
-        return reverse(head).getKey();
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
