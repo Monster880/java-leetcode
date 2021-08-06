7f636b371a06e961b6bd6c85db9726add8d6a9a0
liding <539561425@qq.com>
2021-08-06 23:47:27 +0800

    feature: 双指针

--- leetcode/1574.java
+++ leetcode/1574.java
@@ -8,19 +8,19 @@ class Solution {
         while(left < n -1 && arr[left] <= arr[left+1]){
             left++;
         }
-        if(left == n-1){
+        if(left == n -1){
             return 0;
         }
         int right = n -1;
         while(right > 0 && arr[right] >= arr[right-1]){
             right--;
         }
-        int res = Math.min((n-1) - (left + 1)+1, right);
+        int res = Math.min((n-1)-left, right);
         int i = 0;
         int j = right;
         while(i <= left && j <= n-1){
             if(arr[i] <= arr[j]){
-                res = Math.min(res, j-i-1);
+                res = Math.min(res, (j-i-1));
                 i++;
             }else{
                 j++;
