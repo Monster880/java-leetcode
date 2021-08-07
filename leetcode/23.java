069dcc92427ff13ce619f023f9db653ff517af2e
liding <539561425@qq.com>
2021-08-07 23:16:28 +0800

    fix: 修改代码

--- leetcode/23.java
+++ leetcode/23.java
@@ -9,29 +9,29 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) { 
-        int k = lists.length;
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        while (true) {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int length = lists.length;
+        ListNode dummyNode = new ListNode(0);
+        ListNode tail = dummyNode;
+        while(true){
             ListNode minNode = null;
             int minPointer = -1;
-            for (int i = 0; i < k; i++) {
-                if (lists[i] == null) {
+            for(int i=0;i < length;i++){
+                if(lists[i] == null){
                     continue;
                 }
-                if (minNode == null || lists[i].val < minNode.val) {
+                if(minNode == null || lists[i].val < minNode.val){
                     minNode = lists[i];
                     minPointer = i;
                 }
             }
-            if (minPointer == -1) {
+            if(minPointer == -1){
                 break;
             }
             tail.next = minNode;
             tail = tail.next;
             lists[minPointer] = lists[minPointer].next;
         }
-        return dummyHead.next;
+        return dummyNode.next;
     }
 }
