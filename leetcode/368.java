de4046fd4ead877ffb0ad439f386e73475a8430a
Monster880 <539561425@qq.com>
2021-08-16 00:07:28 +0800

    feature: 动态规划

---
+++ leetcode/368.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        int maxSize = 1;
+        int maxVal = dp[0];
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            if (dp[i] > maxSize) {
+                maxSize = dp[i];
+                maxVal = nums[i];
+            }
+        }
+
+        List<Integer> res = new ArrayList<Integer>();
+        if (maxSize == 1) {
+            res.add(nums[0]);
+            return res;
+        }
+        
+        for (int i = len - 1; i >= 0 && maxSize > 0; i--) {
+            if (dp[i] == maxSize && maxVal % nums[i] == 0) {
+                res.add(nums[i]);
+                maxVal = nums[i];
+                maxSize--;
+            }
+        }
+        return res;
+    }
+}
