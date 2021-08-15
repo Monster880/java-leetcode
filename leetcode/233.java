3bb9b92ae67e13fd5454e06bd9ccb191cccce259
Monster880 <539561425@qq.com>
2021-08-15 23:30:28 +0800

    feature: 数字

---
+++ leetcode/233.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countDigitOne(int n) {
+        long mulk = 1;
+        int ans = 0;
+        for (int k = 0; n >= mulk; ++k) {
+            ans += (n / (mulk * 10)) * mulk + Math.min(Math.max(n % (mulk * 10) - mulk + 1, 0), mulk);
+            mulk *= 10;
+        }
+        return ans;
+    }
+}
