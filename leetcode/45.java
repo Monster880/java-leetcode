c1a5f2ac78635fb46d034280379c13341bfbb9e2
Monster880 <539561425@qq.com>
2021-08-15 23:58:27 +0800

    feature: dp

---
+++ leetcode/45.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+}
