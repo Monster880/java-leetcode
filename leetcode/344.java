d20aab1b124d61209ed22058f725180c7e3d6986
Monster880 <539561425@qq.com>
2021-08-17 22:54:39 +0800

    feature: swap

---
+++ leetcode/344.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int left = 0, right = n - 1; left < right; ++left, --right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+    }
+}
