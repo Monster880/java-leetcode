6105b817ad99e850bebfdf9f0ade581639381d87
Monster880 <539561425@qq.com>
2021-08-19 21:38:10 +0800

    feature: 加入链表

---
+++ leetcode/143.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null){
+            return;
+        }
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while(node != null){
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while(i < j){
+            list.get(i).next = list.get(j);
+            i++;
+            if(i == j){
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--; 
+        }
+        list.get(i).next = null;
+    }
+}
