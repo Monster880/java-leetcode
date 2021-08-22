68ce42913586d8ce7b1806b2a8f19aa07dac978e
Monster880 <539561425@qq.com>
2021-08-22 15:30:03 +0800

    feature: 两次遍历

--- leetcode/135.java
+++ leetcode/135.java
@@ -1,22 +1,22 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int res = 1;
-        int inc = 1, pre = 1, dec = 0;
-        for(int i = 1;i < n; i++){
-            if(ratings[i] >= ratings[i-1]){
-                dec = 0;
-                pre = ratings[i] == ratings[i-1] ? 1 : pre+1;
-                res += pre;
-                inc = pre; 
-            }else{
-                dec++;
-                if(dec == inc){
-                    dec++;
-                }
-                res += dec;
-                pre =1;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
             }
+        }
+        int right = 0, res = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            res += Math.max(left[i], right);
         }
         return res;
     }
