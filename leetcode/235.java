8b8d242e40557dd327c67732ac27bb418300c3a3
Monster880 <539561425@qq.com>
2021-08-22 18:53:36 +0800

    feature: 遍历

---
+++ leetcode/235.java
@@ -0,0 +1,15 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}
