2ab308c44a00fef8ea505d098eb048926fcbf2a6
Monster880 <539561425@qq.com>
2021-08-22 19:53:53 +0800

    feature: 动态规划

--- leetcode/403.java
+++ leetcode/403.java
@@ -3,23 +3,23 @@ class Solution {
         int n = stones.length;
         boolean[][] dp = new boolean[n][n];
         dp[0][0] = true;
-        for (int i = 1; i < n; ++i) {
-            if (stones[i] - stones[i - 1] > i) {
+        for(int i=1; i<n; i++){
+            if(stones[i] - stones[i-1] > i){
                 return false;
             }
         }
-        for (int i = 1; i < n; ++i) {
-            for (int j = i - 1; j >= 0; --j) {
+        for(int i=1; i<n; i++){
+            for(int j=i-1; j>=0; j--){
                 int k = stones[i] - stones[j];
-                if (k > j + 1) {
+                if(k > j+1){
                     break;
                 }
-                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
-                if (i == n - 1 && dp[i][k]) {
+                dp[i][k] = dp[j][k-1] || dp[j][k] || dp[j][k+1];
+                if(i == n-1 && dp[i][k]){
                     return true;
                 }
             }
         }
-        return false;
+        return false;        
     }
 }
