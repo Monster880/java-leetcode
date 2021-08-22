0597002b47020bf5494b4db84df7a8d61affe3e2
Monster880 <539561425@qq.com>
2021-08-22 19:02:45 +0800

    feature: 递归

--- leetcode/offer68.java
+++ leetcode/offer68.java
@@ -1,23 +1,10 @@
 class Solution {
-
-    private TreeNode ans;
-
-    public Solution() {
-        this.ans = null;
-    }
-
-    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return false;
-        boolean left = dfs(root.left, p, q);
-        boolean right = dfs(root.right, p, q);
-        if ((left && right) || ((root.val == p.val || root.val == q.val) && (left || right))) {
-            ans = root;
-        } 
-        return left || right || (root.val == p.val || root.val == q.val);
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        dfs(root, p, q);
-        return ans;
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null) return right;
+        if(right == null) return left;
+        return root;
     }
 }
