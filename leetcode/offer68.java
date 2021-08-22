e9da3e09f31eb78f5636aa5410198e7df43c3e3e
Monster880 <539561425@qq.com>
2021-08-22 18:58:09 +0800

    feature: 地柜

---
+++ leetcode/offer68.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    private TreeNode ans;
+
+    public Solution() {
+        this.ans = null;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean left = dfs(root.left, p, q);
+        boolean right = dfs(root.right, p, q);
+        if ((left && right) || ((root.val == p.val || root.val == q.val) && (left || right))) {
+            ans = root;
+        } 
+        return left || right || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return ans;
+    }
+}
