4900db52406f6fc4f3dc23c1c29a11eea2d331a9
Monster880 <539561425@qq.com>
2021-08-25 23:23:53 +0800

    feature: 动态规划

--- leetcode/offer13.java
+++ leetcode/offer13.java
@@ -8,9 +8,10 @@ class Solution {
         visited[0][0] = true;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if ((i == 0 && j == 0) || get(i) + get(j) > k) {
+                if ((i == 0 && j == 0) || countK(i) + countK(j) > k) {
                     continue;
                 }
+
                 if (i - 1 >= 0) {
                     visited[i][j] |= visited[i - 1][j];
                 }
@@ -23,7 +24,7 @@ class Solution {
         return ans;
     }
 
-    private int get(int x) {
+    private int countK(int x) {
         int res = 0;
         while (x != 0) {
             res += x % 10;
