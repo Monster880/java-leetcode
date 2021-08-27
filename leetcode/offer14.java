dfef38c5b6c3a337d769eb81cf6d7beafed17782
Monster880 <539561425@qq.com>
2021-08-27 23:48:42 +0800

    fix: 修改代码

--- leetcode/offer14.java
+++ leetcode/offer14.java
@@ -7,7 +7,7 @@ class Solution {
         dp[3] = 3;
         dp[4] = 4;
         for(int i=5;i<n+1;i++){
-            dp[i]=Math.max(dp[i-1],Math.max(dp[i-2]*2,dp[i-3]*3));
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]*2, dp[i-3]*3));
         }
         return dp[n];
     }
