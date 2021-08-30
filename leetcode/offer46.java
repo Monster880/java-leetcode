ac9ed5b84d5fefe945dee60c433446ee9ae71c9a
Monster880 <539561425@qq.com>
2021-08-31 01:11:18 +0800

    feature: 回溯

--- leetcode/offer46.java
+++ leetcode/offer46.java
@@ -10,21 +10,19 @@ class Solution {
         return res.toArray(new String[s.length()]);
     }
 
-    void dfs(char[] temp, boolean[] visited, int depth){
-        if(depth == temp.length){
+    private void dfs(char[] temp, boolean[] visited, int depth){
+        if(temp.length == depth){
             res.add(sb.toString());
             return;
         }
-        for(int i = 0; i < temp.length; i++){
-            if(visited[i])  continue;
-            if(i > 0 && temp[i - 1] == temp[i] && !visited[i - 1])  continue;
-            
+        for(int i=0;i<temp.length;i++){
+            if(visited[i]) continue;
+            if(i > 0 && temp[i] == temp[i-1] && !visited[i-1]) continue;
             sb.append(temp[i]);
             visited[i] = true;
-            dfs(temp, visited, depth + 1);
+            dfs(temp, visited, depth+1);
             visited[i] = false;
             sb.deleteCharAt(sb.length() - 1);
         }
     }
-
 }
