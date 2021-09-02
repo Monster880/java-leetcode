8b03b10401c87134281c342ec940c60b5b2cc270
Monster880 <539561425@qq.com>
2021-09-03 00:06:23 +0800

    feature: dp

--- leetcode/offer46.go
+++ leetcode/offer46.go
@@ -1,16 +1,25 @@
 func translateNum(num int) int {
-	src := strconv.Itoa(num)
-	p, q, r := 0, 0, 1
-	for i := 0; i < len(src); i++ {
-		p, q, r = q, r, 0
-		r += q
-		if i == 0 {
-			continue
-		}
-		pre := src[i-1 : i+1]
-		if pre <= "25" && pre >= "10" {
-			r += p
+	if num < 10 {
+		return 1
+	}
+	if num >= 10 && num <= 25 {
+		return 2
+	}
+	str := strconv.Itoa(num)
+	dp := make([]int, len(str))
+	dp[0] = 1
+	if str[:2] >= "10" && str[:2] <= "25" {
+		dp[1] = 2
+	} else {
+		dp[1] = 1
+	}
+	for i := 2; i < len(str); i++ {
+		newnum := str[i-1 : i+1]
+		if newnum >= "10" && newnum <= "25" {
+			dp[i] = dp[i-1] + dp[i-2]
+		} else {
+			dp[i] = dp[i-1]
 		}
 	}
-	return r
+	return dp[len(str)-1]
 }
