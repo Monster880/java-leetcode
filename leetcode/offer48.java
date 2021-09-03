bb0cf9101da5cf8418259adf66ca36e5a603ed62
Monster880 <539561425@qq.com>
2021-09-03 23:55:18 +0800

    feature: 双指针和hash

---
+++ leetcode/offer48.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int i = -1, res = 0;
+        for(int j = 0; j < s.length(); j++) {
+            if(map.containsKey(s.charAt(j))){
+                i = Math.max(i, map.get(s.charAt(j)));
+            }
+            map.put(s.charAt(j), j);
+            res = Math.max(res, j - i);
+        }
+        return res;
+    }
+}
