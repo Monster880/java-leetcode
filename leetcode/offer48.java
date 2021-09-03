1a996dd1847fc3bfd81a5241bf47aee952a80d37
Monster880 <539561425@qq.com>
2021-09-03 23:58:50 +0800

    feature: 双指针和hash

--- leetcode/offer48.java
+++ leetcode/offer48.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
         int i = -1, res = 0;
         for(int j = 0; j < s.length(); j++) {
             if(map.containsKey(s.charAt(j))){
