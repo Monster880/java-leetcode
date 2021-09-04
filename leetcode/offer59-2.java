0a4835e3a66cc741c26ae0c8fe7348468219ae87
Monster880 <539561425@qq.com>
2021-09-04 23:48:52 +0800

    feature: 队列

---
+++ leetcode/offer59-2.java
@@ -0,0 +1,35 @@
+class MaxQueue {
+    Queue<Integer> q;
+    Deque<Integer> d;
+
+    public MaxQueue() {
+        q = new LinkedList<Integer>();
+        d = new LinkedList<Integer>();
+    }
+    
+    public int max_value() {
+        if (d.isEmpty()) {
+            return -1;
+        }
+        return d.peekFirst();
+    }
+    
+    public void push_back(int value) {
+        while (!d.isEmpty() && d.peekLast() < value) {
+            d.pollLast();
+        }
+        d.offerLast(value);
+        q.offer(value);
+    }
+    
+    public int pop_front() {
+        if (q.isEmpty()) {
+            return -1;
+        }
+        int ans = q.poll();
+        if (ans == d.peekFirst()) {
+            d.pollFirst();
+        }
+        return ans;
+    }
+}
