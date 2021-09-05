b19635225c7c5904a2219725f88d0a18a7243815
Monster880 <539561425@qq.com>
2021-09-05 23:15:34 +0800

    feature: 回溯

--- leetcode/22.java
+++ leetcode/22.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<String>();
-        backtrack(ans, new StringBuilder(), 0, 0, n);
-        return ans;
+        List<String> res = new ArrayList<String>();
+        backtrack(res, new StringBuilder(), 0, 0, n);
+        return res;
     }
 
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+    public void backtrack(List<String> res, StringBuilder cur, int open, int close, int max) {
         if (cur.length() == max * 2) {
-            ans.add(cur.toString());
+            res.add(cur.toString());
             return;
         }
         if (open < max) {
             cur.append('(');
-            backtrack(ans, cur, open + 1, close, max);
+            backtrack(res, cur, open + 1, close, max);
             cur.deleteCharAt(cur.length() - 1);
         }
         if (close < open) {
             cur.append(')');
-            backtrack(ans, cur, open, close + 1, max);
+            backtrack(res, cur, open, close + 1, max);
             cur.deleteCharAt(cur.length() - 1);
         }
     }
