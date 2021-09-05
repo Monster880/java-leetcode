70a5fb123a4dc1bf6a6e7fcc596481fc7335da82
Monster880 <539561425@qq.com>
2021-09-05 23:31:08 +0800

    feature: 合并区间

--- leetcode/56.java
+++ leetcode/56.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 0) {
+        if(intervals.length == 0){
             return new int[0][2];
         }
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] interval1, int[] interval2) {
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] interval1, int[] interval2){
                 return interval1[0] - interval2[0];
             }
         });
         List<int[]> merged = new ArrayList<int[]>();
-        for (int i = 0; i < intervals.length; ++i) {
-            int L = intervals[i][0], R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
-                merged.add(new int[]{L, R});
-            } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+        for(int i=0;i<intervals.length;i++){
+            int left = intervals[i][0], right = intervals[i][1];
+            if(merged.size() == 0 || merged.get(merged.size()- 1)[1] < left){
+                merged.add(new int[]{left, right});
+            }else{
+                merged.get(merged.size()-1)[1] = Math.max(merged.get(merged.size()-1)[1], right);
             }
         }
         return merged.toArray(new int[merged.size()][]);
