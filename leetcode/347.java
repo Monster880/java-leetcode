e7cb25d1986b46c92defaa8f2ff1513dc05c082d
Monster880 <539561425@qq.com>
2021-09-12 23:38:41 +0800

    fix: 修改代码

--- leetcode/347.java
+++ leetcode/347.java
@@ -1,34 +1,43 @@
 class Solution {
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
-        HashMap<Integer,Integer> map = new HashMap();
-        for(int num : nums){
-            if (map.containsKey(num)) {
-               map.put(num, map.get(num) + 1);
-             } else {
-                map.put(num, 1);
-             }
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
         }
-        // 遍历map，用最小堆保存频率最大的k个元素
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return map.get(a) - map.get(b);
+
+        List<int[]> values = new ArrayList<int[]>();
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            values.add(new int[]{num, count});
+        }
+        int[] ret = new int[k];
+        qsort(values, 0, values.size() - 1, ret, 0, k);
+        return ret;
+    }
+
+    public void qsort(List<int[]> values, int start, int end, int[] ret, int retIndex, int k) {
+        int picked = (int) (Math.random() * (end - start + 1)) + start;
+        Collections.swap(values, picked, start);
+        
+        int pivot = values.get(start)[1];
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (values.get(i)[1] >= pivot) {
+                Collections.swap(values, index + 1, i);
+                index++;
             }
-        });
-        for (Integer key : map.keySet()) {
-            if (queue.size() < k) {
-                queue.add(key);
-            } else if (map.get(key) > map.get(queue.peek())) {
-                queue.remove();
-                queue.add(key);
+        }
+        Collections.swap(values, start, index);
+
+        if (k <= index - start) {
+            qsort(values, start, index - 1, ret, retIndex, k);
+        } else {
+            for (int i = start; i <= index; i++) {
+                ret[retIndex++] = values.get(i)[0];
+            }
+            if (k > index - start + 1) {
+                qsort(values, index + 1, end, ret, retIndex, k - (index - start + 1));
             }
         }
-        // 取出最小堆中的元素
-        List<Integer> res = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            res.add(queue.remove());
-        }
-        return res;
     }
 }
