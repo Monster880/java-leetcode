c9c2f89dd4e809b325510e18cd86e140cf04277c
Monster880 <539561425@qq.com>
2021-09-13 00:27:05 +0800

    feature: stringbuffer

--- leetcode/1556.java
+++ leetcode/1556.java
@@ -1,17 +1,17 @@
 class Solution {
     public String thousandSeparator(int n) {
         int count = 0;
-        StringBuffer ans = new StringBuffer();
-        do {
+        StringBuffer sb = new StringBuffer();
+        do{
             int cur = n % 10;
             n /= 10;
-            ans.append(cur);
-            ++count;
-            if (count % 3 == 0 && n != 0) {
-                ans.append('.');
+            sb.append(cur);
+            count++;
+            if(count % 3 == 0 && n != 0){
+                sb.append(".");
             }
-        } while (n != 0);
-        ans.reverse();
-        return ans.toString();
+        }while(n != 0);
+        sb.reverse();
+        return sb.toString();
     }
 }
