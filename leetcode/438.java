5d659d104faf3193d40ad79693857e8f53dcf983
Monster880 <539561425@qq.com>
2021-09-13 00:15:04 +0800

    feature: 双指针

--- leetcode/438.java
+++ leetcode/438.java
@@ -4,20 +4,20 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         if(n < m) return res;
 
-        int[] pCnt = new int[26];
-        int[] sCnt = new int[26];
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
 
         for(int i = 0; i < m; i++){
-            pCnt[p.charAt(i) - 'a'] ++;
+            pCount[p.charAt(i) - 'a'] ++;
         }
 
         int left = 0;
         for(int right = 0; right < n; right++){
             int curRight = s.charAt(right) - 'a';
-            sCnt[curRight]++;
-            while(sCnt[curRight] > pCnt[curRight]){
+            sCount[curRight]++;
+            while(sCount[curRight] > pCount[curRight]){
                 int curLeft = s.charAt(left) - 'a';
-                sCnt[curLeft]--;
+                sCount[curLeft]--;
                 left++;
             }
             if(right - left + 1 == m){
