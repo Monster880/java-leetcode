2108388585746a5261d1fed5ed154266a05addf8
Monster880 <539561425@qq.com>
2021-09-16 23:51:10 +0800

    feature: 双指针和数组

---
+++ leetcode/234.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<Integer>();
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
