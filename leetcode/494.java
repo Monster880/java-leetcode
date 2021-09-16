be6de51f482262068d309a51b7d7cb96f5a00a66
Monster880 <539561425@qq.com>
2021-09-16 23:51:34 +0800

    feature: backtrack

---
+++ leetcode/494.java
@@ -0,0 +1,19 @@
+class Solution {
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+
+    public void backtrack(int[] nums, int target, int index, int sum) {
+        if (index == nums.length) {
+            if (sum == target) {
+                count++;
+            }
+        } else {
+            backtrack(nums, target, index + 1, sum + nums[index]);
+            backtrack(nums, target, index + 1, sum - nums[index]);
+        }
+    }
+}
