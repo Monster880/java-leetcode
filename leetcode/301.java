5bfcdb9784a7bff7f6659ff4feba934b0cbec528
Monster880 <539561425@qq.com>
2021-09-17 15:19:07 +0800

    fix: 修改代码

--- leetcode/301.java
+++ leetcode/301.java
@@ -1,19 +1,18 @@
 public class Solution {
-
     private int len;
-    private char[] charArray;
-    private Set<String> validExpressions = new HashSet<>();
+    private char[] sArray;
+    private Set<String> set = new HashSet<>();
 
     public List<String> removeInvalidParentheses(String s) {
-        this.len = s.length();
-        this.charArray = s.toCharArray();
-
+        sArray = s.toCharArray();
+        len = sArray.length;
         int leftRemove = 0;
         int rightRemove = 0;
+
         for (int i = 0; i < len; i++) {
-            if (charArray[i] == '(') {
+            if (sArray[i] == '(') {
                 leftRemove++;
-            } else if (charArray[i] == ')') {
+            } else if (sArray[i] == ')') {
                 if (leftRemove == 0) {
                     rightRemove++;
                 }
@@ -23,35 +22,35 @@ public class Solution {
             }
         }
 
-        StringBuilder path = new StringBuilder();
-        dfs(0, 0, 0, leftRemove, rightRemove, path);
-        return new ArrayList<>(this.validExpressions);
+        StringBuilder sb = new StringBuilder();
+        dfs(0, 0, 0, leftRemove, rightRemove, sb);
+        return new ArrayList<>(set);
     }
 
-    private void dfs(int index, int leftCount, int rightCount, int leftRemove, int rightRemove, StringBuilder path) {
+    private void dfs(int index, int leftCount, int rightCount, int leftRemove, int rightRemove, StringBuilder sb){
         if (index == len) {
             if (leftRemove == 0 && rightRemove == 0) {
-                validExpressions.add(path.toString());
+                set.add(sb.toString());
             }
             return;
         }
 
-        char character = charArray[index];
-        if (character == '(' && leftRemove > 0) {
-            dfs(index + 1, leftCount, rightCount, leftRemove - 1, rightRemove, path);
+        char ch = sArray[index];
+        if (ch == '(' && leftRemove > 0) {
+            dfs(index + 1, leftCount, rightCount, leftRemove - 1, rightRemove, sb);
         }
-        if (character == ')' && rightRemove > 0) {
-            dfs(index + 1, leftCount, rightCount, leftRemove, rightRemove - 1, path);
+        if (ch == ')' && rightRemove > 0) {
+            dfs(index + 1, leftCount, rightCount, leftRemove, rightRemove - 1, sb);
         }
 
-        path.append(character);
-        if (character != '(' && character != ')') {
-            dfs(index + 1, leftCount, rightCount, leftRemove, rightRemove, path);
-        } else if (character == '(') {
-            dfs(index + 1, leftCount + 1, rightCount, leftRemove, rightRemove, path);
+        sb.append(ch);
+        if (ch != '(' && ch != ')') {
+            dfs(index + 1, leftCount, rightCount, leftRemove, rightRemove, sb);
+        } else if (ch == '(') {
+            dfs(index + 1, leftCount + 1, rightCount, leftRemove, rightRemove, sb);
         } else if (rightCount < leftCount) {
-            dfs(index + 1, leftCount, rightCount + 1, leftRemove, rightRemove, path);
+            dfs(index + 1, leftCount, rightCount + 1, leftRemove, rightRemove, sb);
         }
-        path.deleteCharAt(path.length() - 1);
+        sb.deleteCharAt(sb.length() - 1);
     }
 }
