3740466bea0771e8da9e2f02231d40bbf119511c
liding <539561425@qq.com>
2021-09-19 13:27:25 +0800

    feature: 双指针

--- leetcode/283.java
+++ leetcode/283.java
@@ -1,8 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length, left = 0, right = 0;
-        while (right < n) {
-            if (nums[right] != 0) {
+        while(right < n){
+            if(nums[right] != 0){
                 swap(nums, left, right);
                 left++;
             }
@@ -10,7 +10,7 @@ class Solution {
         }
     }
 
-    public void swap(int[] nums, int left, int right) {
+    private void swap(int[] nums, int left, int right){
         int temp = nums[left];
         nums[left] = nums[right];
         nums[right] = temp;
