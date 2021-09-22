a0493dde4bdf80e46f34b74c86a9d1848a8fff35
liding <539561425@qq.com>
2021-09-22 19:52:17 +0800

    fix: 修改带啊

--- leetcode/11.java
+++ leetcode/11.java
@@ -1,17 +1,16 @@
 public class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
-        int ans = 0;
-        while (left < right) {
+        int res = 0;
+        while(left < right){
             int area = Math.min(height[left], height[right]) * (right - left);
-            ans = Math.max(ans, area);
-            if (height[left] <= height[right]) {
-                ++left;
-            }
-            else {
-                --right;
+            res = Math.max(area, res);
+            if(height[left] <= height[right]){
+                left++;
+            }else{
+                right--;
             }
         }
-        return ans;
+        return res;
     }
 }
