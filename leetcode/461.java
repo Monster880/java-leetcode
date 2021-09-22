cd43a8287b1e54f11b6313b9ef10c4dcedfd527f
liding <539561425@qq.com>
2021-09-22 19:47:12 +0800

    fix: 修改代码

--- leetcode/461.java
+++ leetcode/461.java
@@ -1,10 +1,10 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int s = x ^ y, ret = 0;
-        while (s != 0) {
-            ret += s & 1;
+        int s = x ^ y, res = 0;
+        while(s != 0){
+            res += s & 1;
             s >>= 1;
         }
-        return ret;
+        return res;
     }
 }
