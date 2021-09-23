afd81ff3dac9e3dba417a6098b224f1a713e69a7
liding <539561425@qq.com>
2021-09-23 14:39:36 +0800

    fix: 修改代码

--- leetcode/114.java
+++ leetcode/114.java
@@ -18,15 +18,15 @@ class Solution {
         List<TreeNode> list = new ArrayList<TreeNode>();
         preorderTraversal(root, list);
         int size = list.size();
-        for (int i = 1; i < size; i++) {
-            TreeNode prev = list.get(i - 1), curr = list.get(i);
-            prev.left = null;
-            prev.right = curr;
+        for(int i=1; i<size; i++){
+            TreeNode pre = list.get(i-1), cur = list.get(i);
+            pre.left = null;
+            pre.right = cur;
         }
     }
 
-    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
-        if (root != null) {
+    public void preorderTraversal(TreeNode root, List<TreeNode> list){
+        if(root != null){
             list.add(root);
             preorderTraversal(root.left, list);
             preorderTraversal(root.right, list);
