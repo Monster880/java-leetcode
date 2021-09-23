cc257ccf1a1abe0d773b0aa8d2dbc47f5d7c062f
liding <539561425@qq.com>
2021-09-23 23:37:23 +0800

    fix: 修改代码

--- leetcode/394.java
+++ leetcode/394.java
@@ -2,27 +2,27 @@ class Solution {
     public String decodeString(String s) {
         return dfs(s, 0)[0];
     }
-    private String[] dfs(String s, int i) {
-        StringBuilder res = new StringBuilder();
+
+    private String[] dfs(String s, int i){
+        StringBuffer res = new StringBuffer();
         int multi = 0;
         while(i < s.length()){
-            if(s.charAt(i) >= '0' && s.charAt(i) <= '9') 
-                multi = multi * 10 + Integer.parseInt(String.valueOf(s.charAt(i))); 
-            else if(s.charAt(i) == '['){
-                String[] tmp = dfs(s, i + 1);
-                i = Integer.parseInt(tmp[0]);
-                while(multi > 0) {
-                    res.append(tmp[1]);
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                multi = multi * 10 + s.charAt(i) - '0';
+            }else if(s.charAt(i) == '['){
+                String[] temp = dfs(s, i+1);
+                i = Integer.parseInt(temp[0]);
+                while(multi > 0){
+                    res.append(temp[1]);
                     multi--;
                 }
-            }
-            else if(s.charAt(i) == ']'){
-                return new String[]{String.valueOf(i),res.toString()};
+            }else if(s.charAt(i) == ']'){
+                return new String[]{String.valueOf(i), res.toString()};
             }else{
                 res.append(String.valueOf(s.charAt(i)));
             }
             i++;
         }
         return new String[]{res.toString()};
-    } 
+    }
 }
