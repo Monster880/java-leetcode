f18d331b8e7012b68b1c126c61ffeb2adc3a91f5
liding <539561425@qq.com>
2021-09-23 23:52:17 +0800

    feature: 位运算

---
+++ leetcode/338.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] bits = new int[n + 1];
+        int highBit = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                highBit = i;
+            }
+            bits[i] = bits[i - highBit] + 1;
+        }
+        return bits;
+    }
+}
