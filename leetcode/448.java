576c624c4e61e0e4cd9db3a6a6ad2fe0271344cb
liding <539561425@qq.com>
2021-09-25 23:48:21 +0800

    feature: hash

--- leetcode/448.java
+++ leetcode/448.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
-        for (int num : nums) {
+        for(int num : nums){
             int x = (num - 1) % n;
             nums[x] += n;
         }
-        List<Integer> ret = new ArrayList<Integer>();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= n) {
-                ret.add(i + 1);
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i=0; i < n; i++){
+            if(nums[i] <= n){
+                res.add(i+1);
             }
         }
-        return ret;
+        return res;
     }
 }
