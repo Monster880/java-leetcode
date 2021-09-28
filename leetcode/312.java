b1543239fafaede2108a0ceab492d505f2847898
liding <539561425@qq.com>
2021-09-29 01:03:52 +0800

    fix: 修改代码

--- leetcode/312.java
+++ leetcode/312.java
@@ -1,33 +1,33 @@
 class Solution {
-    public int[][] rec;
+    public int[][] res;
     public int[] val;
 
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        val = new int[n + 2];
-        for (int i = 1; i <= n; i++) {
-            val[i] = nums[i - 1];
+        val = new int[n+2];
+        for(int i=1; i <= n; i++){
+            val[i] = nums[i-1];
+        }        
+        val[0] = val[n+1] = 1;
+        res = new int[n+2][n+2];
+        for(int i=0; i<=n+1; i++){
+            Arrays.fill(res[i], -1);
         }
-        val[0] = val[n + 1] = 1;
-        rec = new int[n + 2][n + 2];
-        for (int i = 0; i <= n + 1; i++) {
-            Arrays.fill(rec[i], -1);
-        }
-        return solve(0, n + 1);
+        return dfs(0, n+1);
     }
 
-    public int solve(int left, int right) {
-        if (left >= right - 1) {
+    public int dfs(int left, int right){
+        if(left >= right - 1){
             return 0;
         }
-        if (rec[left][right] != -1) {
-            return rec[left][right];
+        if(res[left][right] != -1){
+            return res[left][right];
         }
-        for (int i = left + 1; i < right; i++) {
+        for(int i=left+1; i < right; i++){
             int sum = val[left] * val[i] * val[right];
-            sum += solve(left, i) + solve(i, right);
-            rec[left][right] = Math.max(rec[left][right], sum);
+            sum += dfs(left, i) + dfs(i, right);
+            res[left][right] = Math.max(res[left][right], sum);
         }
-        return rec[left][right];
+        return res[left][right];
     }
 }
