0b96d68c0a590589daf917e993c70272a0739b1d
liding <539561425@qq.com>
2021-09-29 00:07:26 +0800

    fix: 修改代码

--- leetcode/386.java
+++ leetcode/386.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> res = new ArrayList<Integer>();
         dfs(res, n, 0);
         return res;
     }
 
-    // val是上一位的值
-    private void dfs(List<Integer> res, int n, int val) {
-        for (int i = 0; i <= 9; i++) {
-            if (val == 0 && i == 0) {
+    private void dfs(List<Integer> res, int n, int val){
+        for(int i=0; i<=9; i++){
+            if(val == 0 && i == 0){
                 continue;
             }
-            if (val * 10 + i > n) {
+            if(val * 10 + i > n){
                 return;
             }
             res.add(val * 10 + i);
