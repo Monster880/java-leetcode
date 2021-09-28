71d9bce411d48e26d476e6c31334ceb7828e75f4
liding <539561425@qq.com>
2021-09-29 00:47:26 +0800

    fix: 修改代码

--- leetcode/124.java
+++ leetcode/124.java
@@ -21,13 +21,13 @@ class Solution {
         return maxSum;
     }
 
-    public int dfs(TreeNode node) {
-        if (node == null) {
+    public int dfs(TreeNode node){
+        if(node == null){
             return 0;
         }
-        int leftSum = Math.max(dfs(node.left), 0);
-        int rightSum = Math.max(dfs(node.right), 0);
-        maxSum = Math.max(maxSum, node.val + leftSum + rightSum);
+        int leftSum = Math.max(0, dfs(node.left));
+        int rightSum = Math.max(0, dfs(node.right));
+        maxSum = Math.max(maxSum,node.val + leftSum + rightSum);
         return node.val + Math.max(leftSum, rightSum);
     }
 }
