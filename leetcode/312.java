b5e2ef779fb9889f7de023b8a0a4ddeff3c6e94c
liding <539561425@qq.com>
2021-09-29 00:55:46 +0800

    feature: dfs

---
+++ leetcode/312.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] rec;
+    public int[] val;
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        val = new int[n + 2];
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
+        }
+        val[0] = val[n + 1] = 1;
+        rec = new int[n + 2][n + 2];
+        for (int i = 0; i <= n + 1; i++) {
+            Arrays.fill(rec[i], -1);
+        }
+        return solve(0, n + 1);
+    }
+
+    public int solve(int left, int right) {
+        if (left >= right - 1) {
+            return 0;
+        }
+        if (rec[left][right] != -1) {
+            return rec[left][right];
+        }
+        for (int i = left + 1; i < right; i++) {
+            int sum = val[left] * val[i] * val[right];
+            sum += solve(left, i) + solve(i, right);
+            rec[left][right] = Math.max(rec[left][right], sum);
+        }
+        return rec[left][right];
+    }
+}
