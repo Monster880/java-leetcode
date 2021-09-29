488f589986d5144a88380dd1ec6e7dd35ab24582
liding <539561425@qq.com>
2021-09-30 00:01:00 +0800

    fix: 修改代码

--- leetcode/338.java
+++ leetcode/338.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] bits = new int[n + 1];
-        int highBit = 0;
-        for (int i = 1; i <= n; i++) {
-            if ((i & (i - 1)) == 0) {
-                highBit = i;
+        int[] bits = new int[n+1];
+        int highBits = 0;
+        for(int i=1; i<=n;i++){
+            if((i & (i-1)) == 0){
+                highBits = i;
             }
-            bits[i] = bits[i - highBit] + 1;
+            bits[i] = bits[i-highBits] + 1;
         }
         return bits;
     }
