e749bfdee5e55c04cf65d5a315995f66ea476e41
liding <539561425@qq.com>
2021-09-30 00:33:05 +0800

    fix: 修改代码

--- leetcode/8.java
+++ leetcode/8.java
@@ -2,16 +2,16 @@ class Solution {
     public int myAtoi(String str) {
         Automation automation = new Automation();
         int length = str.length();
-        for(int i=0; i < length; i++){
+        for(int i=0; i<length; i++){
             automation.get(str.charAt(i));
         }
-        return (int)(automation.sign * automation.ans);
+        return (int)(automation.sign * automation.res); 
     }
 }
 
 class Automation{
     public int sign = 1;
-    public long ans = 0;
+    public long res = 0;
     private String state = "start";
     private Map<String, String[]> table = new HashMap<String, String[]>(){{
         put("start", new String[]{"start", "signed", "in_number", "end"});
@@ -23,8 +23,8 @@ class Automation{
     public void get(char c){
         state = table.get(state)[get_col(c)];
         if("in_number".equals(state)){
-            ans = ans * 10 + c - '0';
-            ans = sign == 1 ? Math.min(ans, (long)Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            res = res * 10 + c - '0';
+            res = sign == 1 ? Math.min(res, (long)Integer.MAX_VALUE) : Math.min(res, -(long)Integer.MIN_VALUE);
         }else if("signed".equals(state)){
             sign = c == '+' ? 1 : -1;
         }
