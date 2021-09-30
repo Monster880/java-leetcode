8a18a6325586b2d5494c3a5c0887f79744f3c05e
liding <539561425@qq.com>
2021-09-30 15:23:17 +0800

    feature: 循环

--- leetcode/36.java
+++ leetcode/36.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
+        for(int i=0; i < board.length; i++){
+            for(int j=0; j < board[0].length; j++){
                 if(board[i][j] >= '0' && board[i][j] <= '9'){
                     if(!getEffective(i, j, board)){
                         return false;
@@ -25,7 +25,7 @@ class Solution {
         }
         int row = (i / 3) * 3;
         int column = (j / 3) * 3;
-        for(int k1 = row; k1 < row + 3; k1++){
+        for(int k1= row; k1 < row+3; k1++){
             for(int k2 = column; k2 < column + 3; k2++){
                 if((board[k1][k2] == board[i][j]) && (k1 != i && k2 != j)){
                     return false;
