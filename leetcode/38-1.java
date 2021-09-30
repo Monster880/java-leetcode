e39a8c3abeddb697ef4e6561a66987875e8c7842
liding <539561425@qq.com>
2021-10-01 00:41:39 +0800

    feature: 遍历

---
+++ leetcode/38-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        else {
+            String lastStr = countAndSay(n - 1); // 1 2 1 1
+            StringBuilder ans = new StringBuilder();
+            int i = 0, j = 1, len = lastStr.length();
+            while (j < len) {
+                if (lastStr.charAt(i) != lastStr.charAt(j)) {
+                    ans.append(j - i).append(lastStr.charAt(i));
+                    i = j;
+                }
+                j++;
+            }
+            ans.append(j - i).append(lastStr.charAt(i));
+            return ans.toString();
+        }
+    }
+}
