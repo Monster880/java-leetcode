478a41729b41bacb6cbee100b0647fa311b1b915
liding <539561425@qq.com>
2021-10-03 00:30:37 +0800

    fix: 修改代码

--- leetcode/41.java
+++ leetcode/41.java
@@ -1,22 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0) {
+        for(int i=0; i < n; i++){
+            if(nums[i] <= 0){
                 nums[i] = n + 1;
             }
         }
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             int temp = Math.abs(nums[i]);
-            if (temp <= n) {
+            if(temp <= n){
                 nums[temp - 1] = -Math.abs(nums[temp - 1]);
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                return i + 1;
+        for(int i=0; i < n; i++){
+            if(nums[i] > 0){
+                return i+1;
             }
         }
-        return n + 1;
+        return n+1;
     }
 }
