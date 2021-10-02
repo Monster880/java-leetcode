4bfc8851ca9938dce65c8a23d60fc6dc3cfc72d2
liding <539561425@qq.com>
2021-10-03 00:22:05 +0800

    fix: 修改代码

--- leetcode/91.java
+++ leetcode/91.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] f = new int[n + 1];
-        f[0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            if (s.charAt(i - 1) != '0') {
-                f[i] += f[i - 1];
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i=1; i<=n; i++){
+            if(s.charAt(i - 1) != '0'){
+                dp[i] += dp[i-1];
             }
-            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
-                f[i] += f[i - 2];
+            if(i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i-2) - '0') * 10 + (s.charAt(i-1)) - '0') <= 26){
+                dp[i] += dp[i-2];
             }
         }
-        return f[n];
+        return dp[n];
     }
 }
