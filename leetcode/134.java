dbf78fc445a0ed4ce6118a53b2dda51d39c821b4
liding <539561425@qq.com>
2021-10-08 21:50:03 +0800

    fix: 修改代码

--- leetcode/134.java
+++ leetcode/134.java
@@ -1,23 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int i = 0;
-        while (i < n) {
-            int sumOfGas = 0, sumOfCost = 0;
-            int cnt = 0;
-            while (cnt < n) {
-                int j = (i + cnt) % n;
-                sumOfGas += gas[j];
-                sumOfCost += cost[j];
-                if (sumOfCost > sumOfGas) {
+        int index = 0;
+        while(index < n){
+            int gasSum = 0, costSum = 0;
+            int count = 0;
+            while(count < n){
+                int j = (index + count) % n;
+                gasSum += gas[j];
+                costSum += cost[j];
+                if(costSum > gasSum){
                     break;
                 }
-                cnt++;
+                count++;
             }
-            if (cnt == n) {
-                return i;
-            } else {
-                i = i + cnt + 1;
+            if(count == n){
+                return index;
+            }else{
+                index = index + count + 1;
             }
         }
         return -1;
