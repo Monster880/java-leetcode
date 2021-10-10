fe80b4283769a7d3a4342c235ca9f23f745df068
liding <539561425@qq.com>
2021-10-10 19:57:46 +0800

    fix: 修改代码

--- leetcode/212.java
+++ leetcode/212.java
@@ -1,62 +1,56 @@
 class Solution {
     int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     public List<String> findWords(char[][] board, String[] words) {
         Trie trie = new Trie();
         for (String word : words) {
             trie.insert(word);
         }
-
-        Set<String> ans = new HashSet<String>();
+        Set<String> res = new HashSet<String>();
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
-                dfs(board, trie, i, j, ans);
+                dfs(board, trie, i, j, res);
             }
         }
-
-        return new ArrayList<String>(ans);
+        return new ArrayList<String>(res);
     }
 
-    public void dfs(char[][] board, Trie now, int i1, int j1, Set<String> ans) {
-        if (!now.children.containsKey(board[i1][j1])) {
+    public void dfs(char[][] board, Trie node, int i, int j, Set<String> res) {
+        if (!node.children.containsKey(board[i][j])) {
             return;
         }
-        char ch = board[i1][j1];
-        now = now.children.get(ch);
-        if (!"".equals(now.word)) {
-            ans.add(now.word);
+        char ch = board[i][j];
+        node = node.children.get(ch);
+        if (!"".equals(node.word)) {
+            res.add(node.word);
         }
-
-        board[i1][j1] = '#';
+        board[i][j] = '#';
         for (int[] dir : dirs) {
-            int i2 = i1 + dir[0], j2 = j1 + dir[1];
-            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) {
-                dfs(board, now, i2, j2, ans);
+            int newi = i + dir[0], newj = j + dir[1];
+            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
+                dfs(board, node, newi, newj, res);
             }
         }
-        board[i1][j1] = ch;
+        board[i][j] = ch;
     }
 }
 
 class Trie {
     String word;
     Map<Character, Trie> children;
-    boolean isWord;
-
     public Trie() {
-        this.word = "";
-        this.children = new HashMap<Character, Trie>();
+        word = "";
+        children = new HashMap<Character, Trie>();
     }
 
     public void insert(String word) {
-        Trie cur = this;
+        Trie node = this;
         for (int i = 0; i < word.length(); ++i) {
-            char c = word.charAt(i);
-            if (!cur.children.containsKey(c)) {
-                cur.children.put(c, new Trie());
+            char ch = word.charAt(i);
+            if (!node.children.containsKey(ch)) {
+                node.children.put(ch, new Trie());
             }
-            cur = cur.children.get(c);
+            node = node.children.get(ch);
         }
-        cur.word = word;
+        node.word = word;
     }
 }
