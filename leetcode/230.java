801389c19bc7ca7dd3f9a0f152a9608ec64e8e5d
liding <539561425@qq.com>
2021-10-10 21:11:22 +0800

    fix: 修改代码

--- leetcode/230.java
+++ leetcode/230.java
@@ -14,16 +14,15 @@
  * }
  */
 class Solution {
-    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr) {
-        if (root == null) return arr;
-        inorder(root.left, arr);
-        arr.add(root.val);
-        inorder(root.right, arr);
-        return arr;
-    }
-
     public int kthSmallest(TreeNode root, int k) {
         ArrayList<Integer> nums = inorder(root, new ArrayList<Integer>());
-        return nums.get(k - 1);
+        return nums.get(k-1);
+    }
+    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> res){
+        if(root == null) return res;
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+        return res;
     }
 }
