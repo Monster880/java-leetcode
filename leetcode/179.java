c710132ad52f7b4c3e9d2d5f09cb642f28fe1ea2
liding <539561425@qq.com>
2021-10-10 20:54:19 +0800

    fix: 修改代码

--- leetcode/179.java
+++ leetcode/179.java
@@ -1,30 +1,27 @@
 class Solution {
     public String largestNumber(int[] nums) {
         int n = nums.length;
-        // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
         Integer[] numsArr = new Integer[n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             numsArr[i] = nums[i];
         }
-
         Arrays.sort(numsArr, (x, y) -> {
             long sx = 10, sy = 10;
-            while (sx <= x) {
+            while(sx <= x){
                 sx *= 10;
             }
-            while (sy <= y) {
+            while(sy <= y){
                 sy *= 10;
             }
-            return (int) (-sy * x - y + sx * y + x);
+            return (int)(-sy * x - y + sx * y + x);
         });
-
-        if (numsArr[0] == 0) {
+        if(numsArr[0] == 0){
             return "0";
         }
-        StringBuilder ret = new StringBuilder();
-        for (int num : numsArr) {
-            ret.append(num);
+        StringBuffer sb = new StringBuffer();
+        for(int num : numsArr){
+            sb.append(num);
         }
-        return ret.toString();
+        return sb.toString();
     }
 }
