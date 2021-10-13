dc786c9288ec10f082fafcc8c423c0c54ba1b075
liding <539561425@qq.com>
2021-10-14 00:20:27 +0800

    fix: modified

--- leetcode/204.java
+++ leetcode/204.java
@@ -2,17 +2,17 @@ class Solution {
     public int countPrimes(int n) {
         int[] isPrime = new int[n];
         Arrays.fill(isPrime, 1);
-        int ans = 0;
-        for (int i = 2; i < n; ++i) {
-            if (isPrime[i] == 1) {
-                ans += 1;
-                if ((long) i * i < n) {
-                    for (int j = i * i; j < n; j += i) {
+        int res = 0;
+        for(int i=2; i < n; i++){
+            if(isPrime[i] == 1){
+                res++;
+                if((long)i*i < n){
+                    for(int j=i*i; j<n; j+=i){
                         isPrime[j] = 0;
                     }
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
