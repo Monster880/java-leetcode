3998fc11a6b39236f80732bf8bb812bd3f725159
liding <539561425@qq.com>
2021-10-14 20:50:43 +0800

    fix: 修改代码

--- leetcode/334.java
+++ leetcode/334.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length < 3) return false;
-
-        int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
-        for(int i : nums){
-            if(i > mid) return true;
-            if(i <= min) min = i;
-            else mid = i;
+        if(nums.length < 3){
+            return false;
         }
-
+        int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
+        for(int num : nums){
+            if(num > mid) return true;
+            if(num <= min) min = num;
+            else mid = num;
+        }
         return false;
     }
 }
