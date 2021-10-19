6fdebf51531df5d54db58f07216c234aafa2eeb9
liding <539561425@qq.com>
2021-10-20 00:03:22 +0800

    feature: 暴力

--- leetcode/85.java
+++ leetcode/85.java
@@ -1,29 +1,27 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         int m = matrix.length;
-        if (m == 0) {
+        if(m == 0){
             return 0;
         }
         int n = matrix[0].length;
         int[][] left = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == '1'){
+                    left[i][j] = (j == 0 ? 0 : left[i][j-1]) + 1;
                 }
             }
         }
-
         int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '0') {
+        for(int i=0; i < m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == '0'){
                     continue;
                 }
                 int width = left[i][j];
                 int area = width;
-                for (int k = i - 1; k >= 0; k--) {
+                for(int k=i-1; k >=0; k--){
                     width = Math.min(width, left[k][j]);
                     area = Math.max(area, (i - k + 1) * width);
                 }
